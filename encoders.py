0e6106c5ce774b16105d96944bf4143440cf2cbe
dpes-neupane <dpesn47@gmail.com>
2023-11-19 11:46:53 +0545

    encoders added

---
+++ encoders.py
@@ -0,0 +1,11 @@
+from torch import nn
+from encoder import Encoder
+class Encoders(nn.Module):
+    def __init__(self, layers, **encoder_args):
+        self.layers = layers
+        self.encoders = nn.ModuleList([Encoder(**encoder_args) for _ in range(layers)])
+
+    def forward(self, x):
+        for l in self.encoders:
+            x = l(x)
+        return x
