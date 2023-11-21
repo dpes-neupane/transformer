7f0940734dcfc0b1a21bd3ded76e7405204ac513
Dipesh Neupane <dpesn47@gmail.com>
2023-11-21 14:49:26 +0545

    masked attention added

--- multihead.py
+++ multihead.py
@@ -16,13 +16,18 @@ class Multihead(nn.Module):
         nn.init.xavier_uniform_(self.linear.weight)
         self.linear.bias.data.fill_(0)
         
-    def forward(self, x, ret_att=False):
+    def forward(self, x, ret_att=False, decoder=False):
         batch, seq_length, _ = x.size()
         qkv = self.qkv(x)
         qkv = qkv.reshape(batch, seq_length, 3, self.heads, self.emb_dim//self.heads) #(batch, seq_length, qkv, heads, dim_k)
         qkv = qkv.permute(0, 3, 2, 1, -1) #(batch, heads, qkv, seq_length, dim_k)
         q, k, v = torch.chunk(qkv, dim=2, chunks=3)
-        softmax, values =  self.selfAttention(q, k, v)
+        if not decoder:
+            softmax, values =  self.selfAttention(q, k, v)
+        else:
+            mask = torch.ones(5, 5)
+            torch.triu(mask, diagonal=1, out=mask)
+            softmax, values = self.selfAttention(q, k, v, mask)
         values = values.permute(0, 3, 2, 1, -1) #(batch, seq_length, 1, heads, dim_k)
         values = values.reshape(batch, seq_length, self.emb_dim)
         vals = self.linear(values)
@@ -37,5 +42,13 @@ class Multihead(nn.Module):
         values = torch.matmul(softmax, v)
         return softmax, values
 
-
+    def maskedSelfAttention(self, q, k, v, mask=None):
+        dot = torch.matmul(q, k.transpose(-1, -2))
+        scaled_dots = dot / torch.sqrt(torch.tensor(q.size()[-1]))
+        if mask is not None:
+            mask = torch.unsqueeze(mask, 0)
+            scaled_dots = scaled_dots.masked_fill(mask, -9e15)
+        softmax = torch.nn.functional.softmax(scaled_dots, dim=-1)
+        
+        values = torch.matmul(softmax, v)
             
