0a887104abc4c95f57930b656d6173132d905d7a
dpes-neupane <dpesn47@gmail.com>
2023-11-28 19:22:36 +0545

    transformer class created

---
+++ transformer.py
@@ -0,0 +1,33 @@
+from torch import nn, Tensor
+from typing import Union
+from encoder import Encoders
+from decoder import Decoders
+
+
+
+class Transformer(nn.Module):
+    '''
+    Class for Transformer. 
+    
+    Args:
+        layers: no of encoder and decoder layers
+        input_dim: the dimension of the input embeddings, here it is equal to the emb_dim
+        emb_dim: the dimension of the transformer embeddings
+        heads: no of heads for multihead attention
+        linear_dim: dimension of the FCNN layer
+        dropout: dropout probability.
+
+    Shape:
+        - Input: (seq_length, input_dim), (seq_length, input_dim)
+        - Output: (seq_length, emb_dim)
+
+    '''
+    def __init__(self, layers: int, input_dim: int, emb_dim: int, heads: int, linear_dim: int,  dropout: float=0.0) -> None:
+        super().__init__()
+        self.encoder = Encoders(layers, input_dim=input_dim, emb_dim=emb_dim, heads=heads, linear_dim=linear_dim, dropout=dropout)
+        self.decoder = Decoders(layers, input_dim=input_dim, emb_dim=emb_dim, heads=heads, linear_dim=linear_dim, dropout=dropout)
+        
+    def forward(self, x: Tensor, y: Tensor, mask1: Union[Tensor, None]=None, mask2: Union[Tensor, None]=None) -> Tensor:
+        x = self.encoder(x, mask1)
+        z = self.decoder(x, y, mask1, mask2)
+        return z
